7ddbafd7295477e4d5ec6f719af097136ed039fd
Abhi15062002 <110924771+Abhi15062002@users.noreply.github.com>
2024-11-11 14:04:34 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -25,7 +25,7 @@ const getTotalIsles = function (grid) {
       for (let j = 0; j < cols; j++) {
           if (grid[i][j] === 'L') {  
               dfs(i, j);  
-              islandCount++;  // Increment island count
+              islandCount++;  
           }
       }
   }
